9830c023218044e01babed6ed593d6cc08687725
tu pham <phamtu.110894@gmail.com>
2023-04-01 13:33:06 +0700

    update

--- js/tx.js
+++ js/tx.js
@@ -5,14 +5,14 @@ function startTx() {
         let tx2 = Math.floor(Math.random() * 6) + 1
         let returns = tx + tx1 + tx2
         if (returns > 10) {
-            document.write("<table border=1 width=9%>")
+            document.write("<table border=1 width=15%>")
             document.write("<tr>")
             document.write(`<td>${tx} ${tx1} ${tx2} Tổng: ${returns} <span class="icon">👉</span>Tài</td>`)
             document.write("</tr>")
             document.write("</tr></table>")
             returns = 0
         } else {
-            document.write("<table border=1 width=9%>")
+            document.write("<table border=1 width=15%>")
             document.write("<tr>")
             document.write(`<tr><td>${tx} ${tx1} ${tx2} Tổng: ${returns} <span class="icon">👉</span>Xỉu</td></tr>`)
             document.write("</tr>")
